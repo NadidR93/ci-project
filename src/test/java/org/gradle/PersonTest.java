7ec808e585c03a4d4fd341097b3417ca5d608798
Nadid <Nadid@nadids-mbp.byod.gmu.edu>
2015-04-16 14:23:13 -0400

    third commit

--- src/test/java/org/gradle/PersonTest.java
+++ src/test/java/org/gradle/PersonTest.java
@@ -7,6 +7,6 @@ public class PersonTest {
     @Test
     public void canConstructAPersonWithAName() {
         Person person = new Person("Nadid");
-        assertEquals("Nadid", person.getName());
+        assertEquals("Larry", person.getName());
     }
 }
